e10b1c9429d13b40c77f99bac1f6f1f5613cf970
elyor doniyorov <doniyorovelyor@gmail.com>
2021-08-05 14:23:36 +0500

    Use event listener instead of onclick attr

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-function saveLead() {
-    console.log("Button clicked!")
-}
+let inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener('click', function() {
+    console.log("Button clicked from event listener")
+})

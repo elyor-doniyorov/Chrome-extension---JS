b93012e9bfc31066b84e43bb451166ea4efed634
elyor doniyorov <doniyorovelyor@gmail.com>
2021-08-05 15:27:55 +0500

    Use a for loop to log out myLead

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-let inputBtn = document.getElementById("input-btn")
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+let inputEl = document.getElementById("input-el");
+let inputBtn = document.getElementById("input-btn");
 
 inputBtn.addEventListener('click', function() {
-    console.log("Button clicked from event listener")
+    myLeads.push(inputEl.value)
+    console.log(myLeads);
 })
+
+// Log out the items in the myLeads array using a for loop 
+for (let i = 0; i < myLeads.length; i++) {
+    console.log(myLeads[i])
+}

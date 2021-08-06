1d3e10d21eb45af1cb1b0a84e25e2da82927256b
Elyor Doniyorov <doniyorovelyor@gmail.com>
2021-08-06 13:32:28 +0500

    Make delete btn work

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 let myLeads = [];
-let inputEl = document.getElementById("input-el");
-let inputBtn = document.getElementById("input-btn");
-let ulEl = document.getElementById("ul-el")
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn")
+
 
 let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
-console.log(leadsFromLocalStorage);
+
+deleteBtn.addEventListener('dblclick', function () {
+    localStorage.clear()
+    myLeads = []
+    renderLeads()
+})
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage;

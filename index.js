0475a77522e344bdc838683cc494e19f3208d8d6
elyor doniyorov <doniyorovelyor@gmail.com>
2021-08-06 12:04:24 +0500

    Add <a> tag to make item link

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ let listItems = "";
 
 function renderLeads() {
 for (let i = 0; i < myLeads.length; i++) {
-    listItems += "<li>" + myLeads[i] + "</li>";
+    listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>";
+    console.log(listItems)
 }
 ulEl.innerHTML = listItems;
 }

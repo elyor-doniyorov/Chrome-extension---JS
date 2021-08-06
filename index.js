ded094177c5c6fa9f24f7803d065983533c0b215
elyor doniyorov <doniyorovelyor@gmail.com>
2021-08-06 14:08:10 +0500

    Style the list

--- index.js
+++ index.js
@@ -13,12 +13,15 @@ let listItems = "";
 
 function renderLeads() {
 for (let i = 0; i < myLeads.length; i++) {
-    listItems += `<li>
-                    <a target='_blank' href="${myLeads[i]}">
-                        ${myLeads[i]}
-                    </a>
-                </li>`
-    console.log(listItems)
+    listItems = `
+    <li>
+        <a target='_blank' href="${myLeads[i]}">
+        ${myLeads[i]}
+        </a>
+    </li>
+`
+
 }
-ulEl.innerHTML = listItems;
+ulEl.innerHTML += listItems;
+console.log(listItems)
 }

d98ecc26ee19b6b6913413a37b61e58b1e23efbb
Elyor Doniyorov <doniyorovelyor@gmail.com>
2021-08-06 11:09:22 +0500

    Add localStorage to store leads

--- index.js
+++ index.js
@@ -3,25 +3,31 @@ let inputEl = document.getElementById("input-el");
 let inputBtn = document.getElementById("input-btn");
 let ulEl = document.getElementById("ul-el")
 
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+console.log(leadsFromLocalStorage);
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage;
+    renderLeads();
+}
+
 inputBtn.addEventListener('click', function() {
     myLeads.push(inputEl.value);
     inputEl.value = "";
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
     renderLeads()
-})
-
-let listItems = "";
+});
 
 function renderLeads() {
+let listItems = ""
 for (let i = 0; i < myLeads.length; i++) {
-    listItems = `
+    listItems += `
     <li>
         <a target='_blank' href="${myLeads[i]}">
-        ${myLeads[i]}
+            ${myLeads[i]}
         </a>
     </li>
 `
-
-}
-ulEl.innerHTML += listItems;
-console.log(listItems)
-}
+};
+  ulEl.innerHTML = listItems;
+};
